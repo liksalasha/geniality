291524e92f990b9f56fdf48cd8bea724a82afa27
Isaque T. <102703448+liksalasha@users.noreply.github.com>
2025-10-15 19:11:36 -0300

    3d map automatização

--- scripts/fase_1_3d.gd
+++ scripts/fase_1_3d.gd
@@ -4,6 +4,22 @@ const G29_ID = 1
 @export var next_scene_path: String
 var scene_changed := false
 @onready var change: AnimationPlayer = $player3d/change
+@export var tilemap_2d: TileMap
+@export var block_scene: PackedScene  # cena de um bloco 3D (ex: cubo com textura)
+
+func _ready():
+	var cell_size = tilemap_2d.tile_set.tile_size
+
+	for cell in tilemap_2d.get_used_cells(0):
+		var block = block_scene.instantiate()
+		var pos_2d = tilemap_2d.map_to_local(cell)
+
+		# Corrige o espelhamento vertical
+		block.position = Vector3(pos_2d.x, -pos_2d.y + 16, 0)
+		block.rotation_degrees = Vector3(0, 0, 0)
+		
+		add_child(block)
+
 
 func _input(event):
 	if scene_changed:
